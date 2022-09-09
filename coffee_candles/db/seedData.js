9acde814e8f2e11e455b77c40724c6311f79e8fd
Brittany <brittanylcallus@gmail.com>
2022-09-08 23:13:18 -0500

    untested db tables created rough draft

--- coffee_candles/db/seedData.js
+++ coffee_candles/db/seedData.js
@@ -24,15 +24,29 @@ async function createTables() {
     try {
       console.log("Starting to create tables...");
   
+        
+        //products table: category and photo required?
+        //reviews table optional
+        //check on orders table
+        //handle origional price of orders(products)
       await client.query(`
         CREATE TABLE products (
-          
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(255) UNIQUE NOT NUL,
+            description TEXT NOT NULL,
+            price NUMERIC(7, 2),
+            inventory_quantity INTEGER
         );
         CREATE TABLE users (
-         
+            id SERIAL PRIMARY KEY,
+            email VARCHAR(255) UNIQUE NOT NUL
         );
         CREATE TABLE orders (
-          
+            id SERIAL PRIMARY KEY,
+            product  VARCHAR(255) REFERENCES products(title),
+            "productId" INTEGER REFERENCES products(id),
+            price NUMERIC(7, 2) REFERENCES products(price)
+            quantity INTEGER
         );
       `);
   
