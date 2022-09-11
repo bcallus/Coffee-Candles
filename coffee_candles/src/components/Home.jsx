c392883abd70da80b95d5a2d43224502f8a6920a
Christina Trinh <chrisvt.trinh@gmail.com>
2022-09-10 22:50:37 -0500

    finished styling/render for Home and Navbar

--- coffee_candles/src/components/Home.jsx
+++ coffee_candles/src/components/Home.jsx
@@ -9,19 +9,26 @@ import bgImg from '../assets/cappucino.jpg';
 
 const Home = () => {
   return (
-    <div className='w-full'>
-      <div className='w-full h-full bg-no-repeat'>
+  <div className='w-full'>
+    <div className='w-full h-full bg-no-repeat'>
         <img src={bgImg} className='w-full h-full object-cover absolute' />
-      </div>
-      {/* <div className='absolute flex flex-col py-8 md:min-w-[760px] bottom-[-5%] mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200 border border-slate-300 rounded-xl text-center shadow-xl'></div>
-      <p>Connect with us and let us know how we are doing!</p>
-      <div className='flex-justify-between flex-wrap px-4'>
-        <p className ='flex px-4 py-2 text-slate-500'><CameraIcon className='h-6 text-zinc-600' /> Instagram</p>
-        <p className ='flex px-4 py-2 text-slate-500'><CheckBadgeIcon className='h-6 text-zinc-600' /> Twitter</p>
-        <p className ='flex px-4 py-2 text-slate-500'><FaceSmileIcon className='h-6 text-zinc-600' /> Facebook</p>
-        <p className ='flex px-4 py-2 text-slate-500'><EnvelopeIcon className='h-6 text-zinc-600' /> Email</p>
-      </div> */}
     </div>
+    <div className='absolute flex flex-col py-8 md:min-w-[760px] top-[30%] mx-1 
+        md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200 opacity-80 border border-slate-600 rounded-xl text-center shadow-xl'>
+        <p className='w-full self-center text-5xl p-9 mb-5 font-extrabold'>Coffee for everybody.</p>
+        <button className='border-none w-20 self-center bg-[#2E4030] text-white mb-40'>SHOP</button>
+    </div>
+      <div className='absolute flex flex-row py-2 md:min-w-[760px] bottom-[1%] mx-1 
+        md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200 border border-slate-300 rounded-xl text-center shadow-xl'>
+        <p>Connect with us and let us know how we are doing!</p>
+      <ul className='flex items-center p-4'>
+        <li className ='flex px-1 py-1 text-slate-500'><CameraIcon className='h-6 text-zinc-600' /> Instagram</li>
+        <li className ='flex px-1 py-1 text-slate-500'><CheckBadgeIcon className='h-6 text-zinc-600' /> Twitter</li>
+        <li className ='flex px-1 py-1 text-slate-500'><FaceSmileIcon className='h-6 text-zinc-600' /> Facebook</li>
+        <li className ='flex px-1 py-1 text-slate-500'><EnvelopeIcon className='h-6 text-zinc-600' /> Email</li>
+      </ul>
+    </div>
+  </div>
   );
 };
 
