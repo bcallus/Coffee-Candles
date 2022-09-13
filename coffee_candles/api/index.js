9174030418ede78a7ae7bf162a99cf972a11bc2e
Brittany <brittanylcallus@gmail.com>
2022-09-12 21:11:39 -0500

    usersRouter initialized

--- coffee_candles/api/index.js
+++ coffee_candles/api/index.js
@@ -5,8 +5,16 @@ const apiRouter = require("express").Router();
 //jwt stuff here
 
 //define routes below:
-apiRouter.use("/products", require("./productsRouter"));
+const productsRouter = require('./products');
+apiRouter.use("/products", productsRouter);
+
+const usersRouter = require('./users');
+apiRouter.use("/users", usersRouter);
 
 //add error handlers below:
+apiRouter.use("*", (req, res) => {
+    res.status(404);
+    res.send({ message: "route not found" });
+});
 
 module.exports = apiRouter;
---
+++ coffee_candles/api/users.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const usersRouter = express.Router();
+
+// users api endpoints here
+
+// /register endpoints
+
+// /login enpoints
+
+// /me endpoints
+
+module.exports = usersRouter;
