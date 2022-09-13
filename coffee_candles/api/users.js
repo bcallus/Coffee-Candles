c21f711933c3ce9bf01dec05ec3c6a32d5857aab
Brittany <brittanylcallus@gmail.com>
2022-09-12 21:41:08 -0500

    savepoint

--- coffee_candles/api/users.js
+++ coffee_candles/api/users.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const usersRouter = express.Router();
 
+//require db functions here:
+const {
+	
+} = require("../db");
+
 // users api endpoints here
 
 // /register endpoints
+//usersRouter.post("/signup", ...)
 
 // /login enpoints
+//usersRouter.post("/login", ...)
 
 // /me endpoints
+//usersRouter.get("/me", ...)
 
 module.exports = usersRouter;
