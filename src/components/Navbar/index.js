b6d72532dbc7a24d5d2ff23598654a987904dc30
aresamrit <103473907+aresamrit@users.noreply.github.com>
2022-09-14 22:39:47 -0500

    hoping to keep it showing

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,14 +5,15 @@ import {
   Bars,
   NavMenu,
   NavBtn,
-  NavBtnLink,
+  NavBtnLink
 } from './NavbarElements';
+
 
 const Navbar = () => {
   return (
     <>
       <Nav>
-        <h1>Coffee "N" Tea</h1>
+      <h1>Coffee "N" Tea</h1>
         <Bars />
         <NavMenu>
           <NavLink to='/' activeStyle>
@@ -22,7 +23,7 @@ const Navbar = () => {
             Products
           </NavLink>
           <NavLink to='/about' activeStyle>
-            About
+           About
           </NavLink>
           <NavLink to='/sign-up' activeStyle>
             Sign Up
