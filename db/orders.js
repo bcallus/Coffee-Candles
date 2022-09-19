07ccfa4bc42fd1a3a07c1ac9e55ce586b337065e
Brittany <brittanylcallus@gmail.com>
2022-09-19 13:30:50 -0500

    cartsId reference added to orders table

--- db/orders.js
+++ db/orders.js
@@ -2,16 +2,17 @@ const client = require("./client");
 
 async function createOrder({
     productId,
+    cartsId,
     quantity,
     authenticated,
     price
-    }) {
+  }) {
     try {
       const { rows: [order] } = await client.query(`
-      INSERT INTO orders ("productId", quantity, authenticated, price)
-      VALUES ($1, $2, $3, $4)
+      INSERT INTO orders ("productId", "cartsId", quantity, authenticated, price)
+      VALUES ($1, $2, $3, $4, $5)
       RETURNING *;
-    `, [productId, quantity, authenticated, price]);
+    `, [productId, cartsId, quantity, authenticated, price]);
   
       return order;
     }
