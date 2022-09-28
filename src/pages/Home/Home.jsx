144756be8cf468c298824c14517996de8cf9f168
aresamrit <103473907+aresamrit@users.noreply.github.com>
2022-09-27 21:33:01 -0500

    tidied up css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import './home.css'
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate();
+    
+  const handleClick = () => {
+      navigate(`/products/`);
+  }
+
   return (
   <div className='home'>
     <div className='container'>
       Welcome! Our belief here is acquiring the most high quality coffee beans and tea leaves resourced from all over the world ethically and mindfully, this is coffee for every body!
       <div className='btns'>
-        <button className='coffeebtn'>Coffee</button>
-         - or -
-        <button className='teabtn'>Tea</button>
+        <button className='coffeebtn'onClick={handleClick}>SHOP OUR PRODUCTS</button>
       </div>
     </div>
   </div>
